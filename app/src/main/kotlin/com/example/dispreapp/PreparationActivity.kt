066b0a0e56e5e75b2ffe97801c29be15621a91a0
youyou114 <wheretogetinformation@gmail.com>
2019-10-26 20:56:31 +0900

    事前対策の画面追加

--- app/src/main/kotlin/com/example/dispreapp/PreparationActivity.kt
+++ app/src/main/kotlin/com/example/dispreapp/PreparationActivity.kt
@@ -1,12 +1,25 @@
 package com.example.dispreapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_correspondence.*
+import kotlinx.android.synthetic.main.activity_preparation.*
 
 class PreparationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preparation)
+
+        toItemAction.setOnClickListener {
+            val intent = Intent(this, ItemActivity::class.java)
+            startActivity(intent)
+        }
+
+        toMeasuresAction.setOnClickListener {
+            val intent = Intent(this, MeasuresActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
